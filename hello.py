f43927ecae5b501b70ce7e158847d12242aeb617
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2024-01-21 18:23:53 +0530

    tripple arrow and alt down

--- hello.py
+++ hello.py
@@ -3,8 +3,10 @@ print ("hello world")
 print ("the letter v is")
 print ("error")
 print (5)
+'''
 # print (5+"hello")
 print(5*10)
 print(10/2)
+'''
 print(5 ,"lets go")
 print ("1st line \n2nd line \n3rd line")
