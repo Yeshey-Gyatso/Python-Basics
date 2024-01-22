d9d525b23ab869eee91d5fa52663bd1fb68ae142
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2024-01-22 20:24:07 +0530

    complex data type

--- variables.py
+++ variables.py
@@ -2,6 +2,6 @@ a =1 #store 1 in ram in address a
 b="harry"
 c=True
 d=None
-
-print("the type of a is ",type(a))
+e=complex(4,8)
 print(a,b,c,d)
+print("the type of variables are",type(a),type(b),type(c),type(d),type(e))
