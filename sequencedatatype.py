d21c492c70aaaa42ef1e42fd1377f08ed2f964d4
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2024-01-22 20:59:38 +0530

    everything in python is an object

--- sequencedatatype.py
+++ sequencedatatype.py
@@ -1,4 +1,8 @@
 # list is ordered collection of data types
 #list is collection of different data elements
+#tupples are immutable but same like list
+
+#everything in python is an object
+
 list1=[8,2.3,[-4,5],["apple","banana"]]
-print
+print(list1)
