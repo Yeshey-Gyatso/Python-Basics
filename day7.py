0f380d6dada2c051d631c4fb5f1ee3a287afa733
Yeshey-Gyatso <yesheygyatsoo.yg@gmail.com>
2024-01-24 16:23:19 +0530

    completing day 7 code

---
+++ day7.py
@@ -0,0 +1,9 @@
+a=50
+b=2
+
+print("the value of", a,"+",b,"is",a+b)
+print("the value of", a,"-",b,"is",a-b)
+print("the value of", a,"/",b,"is",a/b)
+print("the value of", a,"%",b,"is",a%b)
+print("the value of", a,"*",b,"is",a*b)
+print("the value of", a,"square",b,"is",a**b)
