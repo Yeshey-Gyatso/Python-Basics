82ae6d4a43d7824a3f6f2d463fb6ab7822b83edb
Yeshey <yesheygyatsoo.ygu@gmail.com>
2024-06-28 14:40:20 +0530

    using ''' dsaf '''

--- voice.py
+++ voice.py
@@ -1,4 +1,6 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("I will speak this text")
+engine.say('''yeshey gyatso lovelovelovelovelove
+           loves loves loves
+             coding''')
 engine.runAndWait()
